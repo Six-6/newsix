5bf16f64c51d6c731d072ed84554489ef70d4788
summer <626379451@qq.com>
2016-09-14 10:22:25 +0800

    志同道合

--- laravel5/app/F_hot.php
+++ laravel5/app/F_hot.php
@@ -1,19 +1,29 @@
 <?php namespace App;
+
 /**
  * 志同道合
  * @褚玉密
  */
 use Illuminate\Database\Eloquent\Model;
 
-class F_hot extends Model {
-    public $table="f_hot";
+class F_hot extends Model
+{
+    public $table = "f_hot";
 
     /**
      * @页面展示
      */
     public static function selAll(){
-        $re=self::get();
+        $re = self::limit(6)->get();
         return $re;
     }
 
+    /**
+     * 数据展示
+     */
+    public static function gets(){
+        $re = self::get();
+        return $re;
+
+    }
 }
